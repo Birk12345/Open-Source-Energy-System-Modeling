6b56518580943717e3e5f2e00d7c30f52023db92
Lukas <e16160@student.tuwien.ac.at>
2024-04-07 09:12:04 -0500

    reformat files

--- Calculator.py
+++ Calculator.py
@@ -2,15 +2,17 @@
 def add(x, y):
     return x + y
 
+
 # Function to subtract two numbers
 def subtract(x, y):
     return x - y
+
 
 # Function to multiply two numbers
 def multiply(x, y):
     return x * y
 
+
 # Function to divide two numbers
 def divide(x, y):
     return x / y
-
--- TestFunctionsOfCalculator.py
+++ TestFunctionsOfCalculator.py
@@ -3,23 +3,24 @@ from Calculator import subtract
 from Calculator import multiply
 from Calculator import divide
 
+
 class TestCalculator:
-    def test_add():
+    def test_add(self):
         assert add(4, 5) == 9
         assert add(5, 2) == 7
         assert add(24, 1) == 25
 
-    def test_subtract():
+    def test_subtract(self):
         assert subtract(3, 1) == 2
         assert subtract(2, 1) == 1
         assert subtract(20, 10) == 10
 
-    def test_multiply():
+    def test_multiply(self):
         assert multiply(6, 2) == 12
         assert multiply(1, 5) == 5
         assert multiply(3, 6) == 18
 
-    def test_divide():
+    def test_divide(self):
         assert divide(4, 2) == 2
         assert divide(10, 5) == 2
         assert divide(50, 2) == 25
